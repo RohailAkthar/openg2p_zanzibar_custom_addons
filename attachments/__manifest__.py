c7ca42666bfad7d5f4a69d21dd40d63040c8bacc
RohailAkthar <aktharohail@gmail.com>
2026-01-07 17:13:24 +0530

    remove module dependency

--- attachments/__manifest__.py
+++ attachments/__manifest__.py
@@ -2,7 +2,7 @@
     "name": "Social Registry: Attachments",
     "version": "17.0.0.0.0",
     "license": "LGPL-3",
-    "depends": ["g2p_registry_individual","g2p_programs"],
+    "depends": ["g2p_registry_individual"],
     "data": [
         "views/attachments_views.xml"
     ],
