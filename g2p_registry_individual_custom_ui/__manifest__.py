7a34198ed43e4c895d5283e50569023248d9b5be
Saraswathi K <saraswathi.k@cyberpwn.com>
2026-02-13 19:51:16 +0530

    Customize Individual form view, remove language, delete hide_partner_lang

---
+++ g2p_registry_individual_custom_ui/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "G2P Individual Custom UI",
+    "version": "1.0",
+    "category": "Registry",
+    "summary": "Custom UI changes for Individual form (image size and name fields width)",
+    "description": "Extends the Individual form view to increase image size and field widths",
+    "author": "Your Name",
+    "depends": ["g2p_registry_individual"],
+    "data": [
+        "views/individual_form_custom.xml",
+    ],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+}
+
--- hide_partner_lang/__manifest__.py
+++
@@ -1,9 +0,0 @@
-{
-    "name": "Hide Language Field – Individual Registrant",
-    "version": "1.0",
-    "depends": ["g2p_registry_individual"],
-    "data": [
-        "views/res_partner_view.xml",
-    ],
-    "installable": True,
-}
