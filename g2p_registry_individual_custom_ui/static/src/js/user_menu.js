772e474d600ff1885978b53e4d2eb72eb8617152
RohailAkthar <aktharohail@gmail.com>
2026-03-30 16:10:47 +0530

    removed doc and support

--- g2p_registry_individual_custom_ui/static/src/js/user_menu.js
+++ g2p_registry_individual_custom_ui/static/src/js/user_menu.js
@@ -3,3 +3,5 @@
 import { registry } from "@web/core/registry";
 
 registry.category("user_menuitems").remove("odoo_account");
+registry.category("user_menuitems").remove("documentation");
+registry.category("user_menuitems").remove("support");
