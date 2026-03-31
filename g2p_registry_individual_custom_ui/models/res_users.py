07844bef641ef76fa0630e0a1778003bbeb5a553
RohailAkthar <aktharohail@gmail.com>
2026-03-31 13:50:04 +0530

    passward issue fix

--- g2p_registry_individual_custom_ui/models/res_users.py
+++ g2p_registry_individual_custom_ui/models/res_users.py
@@ -3,24 +3,20 @@ from odoo import models, fields, api
 class ChangePasswordWizard(models.TransientModel):
     _inherit = 'change.password.wizard'
 
-    user_id_display = fields.Many2one('res.users', string="User Name", readonly=True)
-    user_login_display = fields.Char(string="Email ID", readonly=True)
-    new_passwd_display = fields.Char(string="New Password")
+    user_id_display = fields.Many2one('res.users', string='User', readonly=True)
+    user_login_display = fields.Char(string='User Login', readonly=True)
+    new_passwd_display = fields.Char(string='New Password')
 
     @api.model
     def default_get(self, fields):
         res = super(ChangePasswordWizard, self).default_get(fields)
-        if 'user_ids' in res and res['user_ids']:
-            # In Odoo 17, user_ids is typically a list of (0, 0, vals) or (4, id) 
-            # if we opened from a single user.
-            for command in res['user_ids']:
-                if command[0] == 0:  # (0, 0, vals)
-                    vals = command[2]
-                    res.update({
-                        'user_id_display': vals.get('user_id'),
-                        'user_login_display': vals.get('user_login'),
-                    })
-                    break
+        active_ids = self._context.get('active_ids')
+        if active_ids and len(active_ids) == 1:
+            user = self.env['res.users'].browse(active_ids[0])
+            res.update({
+                'user_id_display': user.id,
+                'user_login_display': user.login,
+            })
         return res
 
     def change_password_button(self):
